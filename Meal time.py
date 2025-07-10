168e692d7af32c36c8d97ab78d1b36ff5f69ba5c
Munchogoose <otalukamunachimso@gmail.com>
2025-07-10 08:35:52 +0100

    Update Meal time.py

--- Meal time.py
+++ Meal time.py
@@ -1,6 +1,6 @@
 """Suppose that you’re in a country where it’s customary to eat breakfast between 7:00 and 8:00,
 lunch between 12:00 and 13:00, and dinner between 18:00 and 19:00.
-This program tells the user what to eat base on their time.
+This program tells the user what to eat base on their time."""
 
 This is a program that prompts a user for a time and outputs whether it’s breakfast time,
 lunchtime, or dinner time. If it’s not time for a meal,
